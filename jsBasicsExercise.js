d29f929c114403c481a6b047d667cdb50d9132f3
Briley Burks <brileyburks@BCs-MBP-4.glasgow-ky.com>
2020-10-16 09:55:06 -0500

    Completed JS Basics Exercise

---
+++ jsBasicsExercise.js
@@ -0,0 +1,20 @@
+// JavaScript Basics Exercise
+
+console.log(13);
+
+
+// Numbers, Strings, Booleans, Null, and Undefined
+
+/*
+Adding(+), Subracting(-), Multiplying(*), Dividing(/), Mondulo(%)
+*/
+
+console.log(7%4);
+
+console.log('I\'M MARY POPPINS, Y\'ALL');
+
+console.log("Spider" + "-" + "man");
+
+console.log("Briley".length + "Burks".length);
+
+console.log("Avengers: Endgame"[16])
